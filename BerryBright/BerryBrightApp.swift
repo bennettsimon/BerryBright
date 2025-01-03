a502e83efca3fea0e2c8ff59b53afbe90b4802ee
bennettsimon <hisimon.me@gmail.com>
2025-01-03 16:32:07 +0800

    Initial Commit

---
+++ BerryBright/BerryBrightApp.swift
@@ -0,0 +1,17 @@
+//
+//  BerryBrightApp.swift
+//  BerryBright
+//
+//  Created by Simon on 03/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct BerryBrightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
